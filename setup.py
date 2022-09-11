24d42cd56130eb4fd10e5ceb85d727027289351d
Alain Saamego <73674079+solobeton99@users.noreply.github.com>
2022-09-11 21:38:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,11 @@
-import setuptools
-
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
-setuptools.setup(
-    name="calvin",
-    version="0.0.1",
-    author="Phil Mohun, Malcolm Navarro, DNE LLC",
-    author_email="calvin@philmohun.com",
-    description="Calvin is a Python library for developers using GPT-3.",
-    long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/pmohun/calvin",
-    download_url="https://github.com/pmohun/calvin/archive/v0.1.1.tar.gz",
-    packages=setuptools.find_packages(),
-    classifiers=[
-        "Programming Language :: Python :: 3",
-    	"License :: OSI Approved :: BSD 3-Clause "New" or "Revised" License (BSD-3-Clause)",
-        "Operating System :: OS Independent",
-    ],
-    python_requires='>=3.6',
-)
+mkdir -p ~/.streamlit/
+echo "\
+[general]\n\
+email = \"{c6lculus8ntr0py@gmail.com}\"\n\
+" > ~/.streamlit/credentials.toml
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
