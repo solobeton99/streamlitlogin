aa70f0a73bd5250065daf3074df76daf7816010d
Alain Saamego <73674079+solobeton99@users.noreply.github.com>
2022-09-11 21:19:37 +0100

    Update test_run.py

--- test_run.py
+++ test_run.py
@@ -1,7 +1,7 @@
 from streamlit_login_auth_ui.widgets import __login__
 import streamlit as st
 
-__login__obj = __login__(auth_token = "courier_auth_token", company_name = "company_name", width = 200, height = 250, logout_button_name = 'Logout', hide_menu_bool = False, hide_footer_bool = False, lottie_url='https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
+__login__obj = __login__(auth_token = "courier_auth_token", company_name = "Greatlack", width = 200, height = 250, logout_button_name = 'Logout', hide_menu_bool = False, hide_footer_bool = False, lottie_url='https://assets2.lottiefiles.com/packages/lf20_jcikwtux.json')
 LOGGED_IN = __login__obj.build_login_ui()
 
 
